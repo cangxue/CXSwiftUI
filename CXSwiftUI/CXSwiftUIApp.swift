39bfce8e1e66470aaf788d567b25f17b42593cc2
xiaoma <752787086@qq.com>
2020-12-01 17:14:27 +0800

    Initial Commit

---
+++ CXSwiftUI/CXSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CXSwiftUIApp.swift
+//  CXSwiftUI
+//
+//  Created by mahong yang on 2020/12/1.
+//
+
+import SwiftUI
+
+@main
+struct CXSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
